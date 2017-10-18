f03c118d0f5aaa025d60556891e9fff4990370ef
Nicklasos <nicklasos@gmail.com>
2017-10-18 12:16:27 +0300

    Update proxy.php

--- php/proxy.php
+++ php/proxy.php
@@ -1,13 +1,17 @@
 <?php
 
-if (($_SERVER['HTTP_PASSWORD'] ?? null) !== 'Password') {
+error_reporting(0);
+
+if (($_SERVER['HTTP_PASSWORD'] ?? null) !== 'ProxyPassword') {
     http_response_code(401);
     exit();
 }
 
 $response = file_get_contents(urldecode($_GET['url']), false, stream_context_create([
     'http' => [
+        'method' => $_SERVER['REQUEST_METHOD'],
         'ignore_errors' => true,
+        'content' => file_get_contents('php://input'),
         'timeout' => 10,
     ],
 ]));
