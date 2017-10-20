9fa4844b9ef891e0df2ef84318385548a2ce10c7
Nicklasos <nicklasos@gmail.com>
2017-10-20 18:05:39 +0300

    Add laravel command to create admin users.

---
+++ php/laravel/Commands/AdminCreate.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Admin;
+use App\Http\Requests\AdminAdminCreateRequest;
+use Illuminate\Console\Command;
+use Validator;
+
+class AdminCreate extends Command
+{
+    protected $signature = 'admin:create {email} {role=admin}';
+    protected $description = 'Create admin user';
+
+    public function handle()
+    {
+        $password = $this->secret('Type password');
+        $repeat = $this->secret('Repeat');
+
+        if ($password !== $repeat) {
+            $this->error('Password mismatch');
+            exit();
+        }
+
+        $validator = Validator::make($this->arguments(), [
+            'email' => 'required|string|email|max:255|unique:admins',
+            'password' => 'required|string|min:6',
+            'role' => 'required|in:admin',
+        ]);
+
+        if ($validator->fails()) {
+            $this->error($validator->errors());
+            exit();
+        }
+
+        $email = $this->argument('email');
+        $role = $this->argument('role');
+
+        Admin::create([
+            'name' => str_before($email, '@'),
+            'email' => $email,
+            'role' => $role,
+            'password' => bcrypt($password),
+        ]);
+
+        $this->info('ok');
+    }
+}
