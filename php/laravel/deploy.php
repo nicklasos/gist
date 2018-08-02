0860d434bb322fdd1dc740c03d522c8123685d6d
Nicklasos <nicklasos@gmail.com>
2018-08-02 12:00:00 +0300

    New deploy config with assets build

--- php/laravel/deploy.php
+++ php/laravel/deploy.php
@@ -1,122 +1,82 @@
 <?php
+
+<?php
+
+namespace Deployer;
+
 /** @noinspection PhpIncludeInspection */
-require 'recipe/common.php';
-
-server('prod', '127.0.0.2')
-    ->env('deploy_path', '/var/www/site')
-    ->user('root')
-    ->identityFile()
-    ->stage('production');
-
-set('repository', 'git@github.com:username/repository.git');
-
-set('shared_dirs', [
-    'storage/app',
-    'storage/framework/cache',
-    'storage/framework/sessions',
-    'storage/framework/views',
-    'storage/logs',
-    'public/boxes',
-    'public/items',
-    'public/sitemap',
-    'public/other'
-]);
-
-set('keep_releases', 25);
-
-set('shared_files', ['.env']);
-
-set('writable_dirs', [
-    'bootstrap/cache',
-    'storage',
-    'storage/logs',
-    'public/boxes',
-    'public/sitemap',
-]);
-
-task('deploy:php:reload', function () {
-    run('sudo service php7.0-fpm reload');
-})->desc('Reload PHP-FPM');
+require 'recipe/laravel.php';
+require 'recipe/yarn.php';
 
 task('artisan:config:clear', function () {
     run('{{bin/php}} {{deploy_path}}/current/artisan config:clear');
-})->desc('Disable maintenance mode');
-
-task('artisan:up', function () {
-    run('{{bin/php}} {{deploy_path}}/current/artisan up');
-})->desc('Disable maintenance mode');
-
-task('artisan:down', function () {
-    run('{{bin/php}} {{deploy_path}}/current/artisan down');
-})->desc('Enable maintenance mode');
-
-task('artisan:migrate', function () {
-    run('{{bin/php}} {{deploy_path}}/current/artisan migrate --force');
-})->desc('Execute artisan migrate');
-
-task('artisan:migrate:rollback', function () {
-    run('{{bin/php}} {{deploy_path}}/current/artisan migrate:rollback --force');
-})->desc('Execute artisan migrate:rollback');
-
-task('artisan:migrate:status', function () {
-    run('{{bin/php}} {{deploy_path}}/current/artisan migrate:status');
-})->desc('Execute artisan migrate:status');
-
-task('artisan:db:seed', function () {
-    run('{{bin/php}} {{deploy_path}}/current/artisan db:seed --force');
-})->desc('Execute artisan db:seed');
-
-task('artisan:cache:clear', function () {
-    run('{{bin/php}} {{deploy_path}}/current/artisan cache:clear');
-})->desc('Execute artisan cache:clear');
-
-task('artisan:config:cache', function () {
-    run('{{bin/php}} {{deploy_path}}/current/artisan config:cache');
-})->desc('Execute artisan config:cache');
-
-task('artisan:route:cache', function () {
-    run('{{bin/php}} {{deploy_path}}/current/artisan route:cache');
-})->desc('Execute artisan route:cache');
-
-task('artisan:route:trans:cache', function () {
-    run('{{bin/php}} {{deploy_path}}/current/artisan route:trans:cache');
-})->desc('Execute artisan route:trans:cache');
-
-task('artisan:view:clear', function () {
-    run('{{bin/php}} {{deploy_path}}/current/artisan view:clear');
-})->desc('Execute artisan route:trans:cache');
+})->desc('Clear config');
 
 task('artisan:queue:restart', function () {
     run('{{bin/php}} {{deploy_path}}/current/artisan queue:restart');
 })->desc('Execute artisan queue:restart');
 
-task('deploy:public_disk', function () {
-    run('if [ -d $(echo {{release_path}}/public/storage) ]; then rm -rf {{release_path}}/public/storage; fi');
-    run('mkdir -p {{deploy_path}}/shared/storage/app/public');
-    run('ln -nfs {{deploy_path}}/shared/storage/app/public {{release_path}}/public/storage');
-})->desc('Make symlink for public disk');
+task('npm:prod', function () {
+    run('cd {{ release_path }} && npm run prod');
+});
 
 task('deploy', [
+    'deploy:info',
     'deploy:prepare',
+    'deploy:lock',
     'deploy:release',
     'deploy:update_code',
     'deploy:shared',
     'deploy:vendors',
     'deploy:writable',
-    'deploy:symlink',
-    'cleanup',
-    //    'artisan:cache:clear',
+    'artisan:storage:link',
     'artisan:view:clear',
-    'artisan:config:cache',
     'artisan:config:clear',
-    'artisan:migrate',
-    'artisan:route:trans:cache',
-    'deploy:php:reload',
+    'yarn:install',
+    'yarn:prod',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
     'artisan:queue:restart',
-])->desc('Deploy project');
+]);
 
-after('deploy', 'success');
+// Configuration
 
-set('copy_dirs', ['vendor']);
+set('repository', 'git@github.com:DrSat/giveaway.git');
+set('git_tty', true); // [Optional] Allocate tty for git on first deployment
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+set('keep_releases', 20);
+
+// Hosts
+
+host('127.0.0.1')
+    ->stage('production')
+    ->set('branch', 'develop')
+    ->identityFile('~/.ssh/id_rsa')
+    ->set('deploy_path', '/var/www/site');
+
+// Tasks
+desc('Restart PHP-FPM service');
+task('deploy:php:reload', function () {
+    run('for s in $( service --status-all | grep -o "\bphp.*fpm\b" ); do sudo service $s restart; done');
+});
+
+desc('Build assets');
+task('yarn:prod', function () {
+    run('cd {{release_path}} && {{bin/yarn}} run production');
+});
+
+after('deploy:symlink', 'deploy:php:reload');
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
 
 before('deploy:vendors', 'deploy:copy_dirs');
